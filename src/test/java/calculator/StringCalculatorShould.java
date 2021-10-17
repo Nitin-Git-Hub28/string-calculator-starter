33df2c82cee2a5c7f81f2a1aebcd53108e27d3d6
nitin <nitin@NITIN-PC1>
2021-10-17 13:28:09 +0530

    Tested for empty string should return 0 and string with
    single number should return number

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -8,13 +8,11 @@ class StringCalculatorShould {
 
     @Test
     void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+        assertEquals(0, StringCalculator.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(1, StringCalculator.add("1"));
     }
 }
